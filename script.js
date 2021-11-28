f29250efc52c3c212bf87d87b4af22445aa96197
diptasahacse <diptasaha.lpu.cse@gmail.com>
2021-11-28 10:59:34 +0600

    add some features

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ function gerRandomName(){
     
     
         var nameList = document.getElementById('inputNameList').value;
-        var nameArray = nameList.split('\n');
-        var drawPosition = Math.floor(Math.random()*nameArray.length);
-        var li = document.createElement('li');
-        li.innerText = nameArray[drawPosition];
-        var ul = document.getElementById('luckyPerson');
 
-        var ulLength = document.getElementById('luckyPerson').childNodes.length
-        if(ulLength < 4){
-            ul.appendChild(li);
-            nameArray.splice(drawPosition,1);
-            document.getElementById('inputNameList').value = arrayToString(nameArray)
+        if(nameList.length){
+            var nameArray = nameList.split('\n');
+            var drawPosition = Math.floor(Math.random()*nameArray.length);
+            var li = document.createElement('li');
+            li.innerText = nameArray[drawPosition];
+            var ul = document.getElementById('luckyPerson');
+
+            var ulLength = document.getElementById('luckyPerson').childNodes.length
+            if(ulLength < 3){
+                ul.appendChild(li);
+                nameArray.splice(drawPosition,1);
+                document.getElementById('inputNameList').value = arrayToString(nameArray)
+
+            }
+            else{
+                alert('Winner is Already Selected')
+
+            }
+
 
         }
         else{
-            alert('Winner is Already Selected')
+            alert('Please Input')
 
         }
+        
         
 
    
